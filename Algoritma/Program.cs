b28aceb998e53e1a18d4234748bb9821e22f8d3d
Ömer Küçük <khuseyinomer@gmail.com>
2025-03-31 17:21:49 +0300

    Algoritma

---
+++ Algoritma/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Lütfen integer çiftlerini boşlukla ayırarak giriniz:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        if (numbers.Length % 2 != 0)
+        {
+            Console.WriteLine("Hatalı giriş! Lütfen çift sayıda değer giriniz.");
+            return;
+        }
+
+        for (int i = 0; i < numbers.Length; i += 2)
+        {
+            int num1 = int.Parse(numbers[i]);
+            int num2 = int.Parse(numbers[i + 1]);
+            int sum = num1 + num2;
+
+            if (num1 == num2)
+                Console.Write($"{sum * sum} ");
+            else
+                Console.Write($"{sum} ");
+        }
+    }
+}
