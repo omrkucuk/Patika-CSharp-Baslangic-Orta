a53838a8b4a0a37428d42d3dc3dcf924fddc6219
Ömer Küçük <khuseyinomer@gmail.com>
2025-03-31 17:24:01 +0300

    Mutlak Kare Alma

---
+++ Mutlak_Kare_Alma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Lütfen sayıları boşlukla ayırarak giriniz:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        int sumSmall = 0, sumLarge = 0;
+        int threshold = 67;
+
+        foreach (string num in numbers)
+        {
+            int value = int.Parse(num);
+            int diff = Math.Abs(value - threshold);
+            
+            if (value < threshold)
+                sumSmall += diff;
+            else
+                sumLarge += diff * diff;
+        }
+
+        Console.WriteLine($"{sumSmall} {sumLarge}");
+    }
+}
